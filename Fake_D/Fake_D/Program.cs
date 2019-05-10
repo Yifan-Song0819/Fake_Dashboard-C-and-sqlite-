aa7a1ab75c9844a3d98cd05a74596877df733c04
James Song <j.codes1989@gmail.com>
2019-05-10 12:30:44 +1200

    the dll killing me

---
+++ Fake_D/Fake_D/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+using System.Data.SQLite;
+
+namespace Fake_D
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+
+
+            DataBase dataBaseObject = new DataBase();
+            dataBaseObject.myConnection.Open();
+
+
+            string myInsertQuery = "INSERT INTO Dean VALUES ('yson','188')";
+            SQLiteCommand sqCommand = new SQLiteCommand(myInsertQuery);
+            sqCommand.Connection = dataBaseObject.myConnection;
+            sqCommand.ExecuteNonQuery();
+            dataBaseObject.myConnection.Close();
+            Console.ReadKey();
+        }
+    }
+}
