cfa06cfa262073ba0d8abcdaef79f939af03d187
YSMS <928039941@qq.com>
2019-06-01 16:53:51 +0800

    need more buttons

--- Fake_Dashboard/Dean.Designer.cs
+++ Fake_Dashboard/Dean.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.DataTable1 = new System.Windows.Forms.DataGridView();
             this.CourseNumComboBox = new System.Windows.Forms.ComboBox();
+            this.AddNewStudentButton = new System.Windows.Forms.Button();
+            this.ShowStatsButton = new System.Windows.Forms.Button();
+            this.ChangeDataButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DataTable1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,11 +55,41 @@
             this.CourseNumComboBox.TabIndex = 1;
             this.CourseNumComboBox.SelectedIndexChanged += new System.EventHandler(this.CourseNumComboBox_SelectedIndexChanged);
             // 
+            // AddNewStudentButton
+            // 
+            this.AddNewStudentButton.Location = new System.Drawing.Point(790, 51);
+            this.AddNewStudentButton.Name = "AddNewStudentButton";
+            this.AddNewStudentButton.Size = new System.Drawing.Size(194, 23);
+            this.AddNewStudentButton.TabIndex = 2;
+            this.AddNewStudentButton.Text = "Add Students To This Course";
+            this.AddNewStudentButton.UseVisualStyleBackColor = true;
+            // 
+            // ShowStatsButton
+            // 
+            this.ShowStatsButton.Location = new System.Drawing.Point(790, 130);
+            this.ShowStatsButton.Name = "ShowStatsButton";
+            this.ShowStatsButton.Size = new System.Drawing.Size(194, 23);
+            this.ShowStatsButton.TabIndex = 3;
+            this.ShowStatsButton.Text = "Show Stats";
+            this.ShowStatsButton.UseVisualStyleBackColor = true;
+            // 
+            // ChangeDataButton
+            // 
+            this.ChangeDataButton.Location = new System.Drawing.Point(790, 91);
+            this.ChangeDataButton.Name = "ChangeDataButton";
+            this.ChangeDataButton.Size = new System.Drawing.Size(194, 23);
+            this.ChangeDataButton.TabIndex = 4;
+            this.ChangeDataButton.Text = "Change Data Of Students";
+            this.ChangeDataButton.UseVisualStyleBackColor = true;
+            // 
             // Dean
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.ChangeDataButton);
+            this.Controls.Add(this.ShowStatsButton);
+            this.Controls.Add(this.AddNewStudentButton);
             this.Controls.Add(this.CourseNumComboBox);
             this.Controls.Add(this.DataTable1);
             this.Name = "Dean";
@@ -72,5 +105,8 @@
 
         private System.Windows.Forms.DataGridView DataTable1;
         private System.Windows.Forms.ComboBox CourseNumComboBox;
+        private System.Windows.Forms.Button AddNewStudentButton;
+        private System.Windows.Forms.Button ShowStatsButton;
+        private System.Windows.Forms.Button ChangeDataButton;
     }
 }
